b649653fc4ab97043633f120adc1965909675941
keyur005 <barvaliyakeyur1998@gmail.com>
2021-09-08 10:31:13 +0530

    add android id

--- android/app/src/main/kotlin/com/example/flutter_appcenter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_appcenter_demo/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.flutter_appcenter_demo
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    AppCenter.start(getApplication(), "ecf5e857-ce28-40af-a4f1-fce3d78a22cb",
+    Analytics.class, Crashes.class);
 }
